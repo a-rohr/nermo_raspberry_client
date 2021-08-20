f237e7a5ad0bb54e35dacff0d5b6973153bea152
Alex Rohregger <ge36tuw@mytum.de>
2021-08-20 19:19:24 +0000

    Update low_level_controller.py to python2.7 status

--- scripts/low_level_controller.py
+++ scripts/low_level_controller.py
@@ -32,7 +32,7 @@ class Low_Level_Controller:
         self.motor_ctrl.shutdown_ctrl()
         print("Shutting down ros node")
 
-    def low_level_controller_loop(self, rate: int) -> None:
+    def low_level_controller_loop(self, rate):
         """ Low level controller ROS loop """
 
         rospy.init_node("low_level_controller", anonymous=True)
