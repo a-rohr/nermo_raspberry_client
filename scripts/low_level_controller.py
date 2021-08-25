6729c4ce8f3c22b3a314dc61870aa4ea804b6e22
Alex Rohregger <alexrohregger96@gmail.com>
2021-08-25 15:00:44 +0200

    reduced rate of low level controller to 30

--- scripts/low_level_controller.py
+++ scripts/low_level_controller.py
@@ -49,7 +49,7 @@ class Low_Level_Controller:
     def main(self):
         """ Main function - try to run the ROS node, otherwise pass """
         try:
-            rate = 50
+            rate = 30
             self.low_level_controller_loop(rate)
         except rospy.ROSInterruptException:
             pass
