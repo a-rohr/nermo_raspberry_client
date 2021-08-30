8ec44eb203f42b501bd8b0d66be144d45df6e7c4
Alex Rohregger <alexrohregger96@gmail.com>
2021-08-30 18:17:03 +0200

    adjusted offsets again for rear legs by lessening of 20 degrees

--- src/nermo_client/MotorCtrl.py
+++ src/nermo_client/MotorCtrl.py
@@ -31,8 +31,8 @@ class Motors(CMouseCom):
                                 ID_HINDRIGHT_KNEE, ID_HINDLEFT_KNEE, ID_FORERIGHT_HIP, ID_SPINE])
         self.offset_q = np.array([ 180.0, 120.0,
                                  180.0, 240.0,
-                                 180.0, 120.0,
-                                 180.0, 240.0,
+                                 180.0, 140.0,
+                                 180.0, 220.0,
                                  180.0, 180.0, 180.0, 180.0])
 
         self.sign_change = np.array([-1.0, 1.0,
